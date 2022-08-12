b6628f82ecd33ec6d5314235f1161e29e7a93bf0
Alexander Hjelm <alexander.hjelm@solidify.dev>
2022-08-12 22:33:48 +0200

    Remove redundant logic from PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,9 +2,6 @@ using Godot;
 
 public class PlayerController : KinematicBody
 {
-	[Export] public int Speed = 10;
-	[Export] public float gravity = 0.7f;
-
 	private Vector3 velocity = new Vector3();
 
 	private Label NameLabel { get; set; }
@@ -14,61 +11,19 @@ public class PlayerController : KinematicBody
 	[Puppet]
 	public Vector3 PuppetVelocity { get; set; }
 
-	public void GetInput()
-	{
-		float ySpeed = velocity.y;
-		velocity = new Vector3();
-
-		if (Input.IsActionPressed("move_right"))
-			velocity.x += 1;
-
-		if (Input.IsActionPressed("move_left"))
-			velocity.x -= 1;
-
-		if (Input.IsActionPressed("move_forward"))
-			velocity.z += 1;
-
-		if (Input.IsActionPressed("move_back"))
-			velocity.z -= 1;
-
-		velocity = velocity.Normalized() * Speed;
-		
-		velocity.y = ySpeed - gravity;
-
-		Rset(nameof(PuppetPosition), Transform.origin);
-		Rset(nameof(PuppetVelocity), velocity);
-	}
-
 	public override void _PhysicsProcess(float delta)
 	{
-		if (IsNetworkMaster())
-		{
-			GetInput();
-		}
-		else
-		{
-			Transform t = Transform;
-			t.origin = PuppetPosition;
-			Transform = t;
+		Transform t = Transform;
+		t.origin = PuppetPosition;
+		Transform = t;
 			
-			velocity = PuppetVelocity;
-		}
-		
+		velocity = PuppetVelocity;
 		velocity = MoveAndSlide(velocity);
-
-		if (!IsNetworkMaster())
-		{
-			PuppetPosition = Transform.origin;
-		}
 	}
 
 	public void SetPlayerName(string name)
 	{
 		NameLabel = (Label)GetNode("LabelOrigin/Viewport/Label");
-
-		PuppetPosition = Transform.origin;
-		PuppetVelocity = velocity;
-
 		NameLabel.Text = name;
 	}
 }
